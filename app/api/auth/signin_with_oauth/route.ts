4a784aa7cf3683524181dacef4e2d50ba1a4439c
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-05-14 14:09:50 +0700

    SignInWithOAuthAPI

---
+++ app/api/auth/signin_with_oauth/route.ts
@@ -0,0 +1,80 @@
+import User from "@/database/user.model";
+import dbConnect from "@/lib/dbConnect";
+import { SignInWithOAuthSchema } from "@/lib/schemas/SignInWithOAuthSchema";
+import slugify from "slugify";
+import mongoose from "mongoose";
+
+export async function POST(req: Request) {
+  try {
+    // restructure data and connect to db
+    const { provider, providerAccountId, user } = await req.json();
+    await dbConnect();
+    // start transaction
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      // validate data
+      const validatedData = SignInWithOAuthSchema.parse({
+        provider,
+        providerAccountId,
+        user,
+      });
+      const { email, name, username, image } = validatedData.user;
+      // check if user exists
+      const existingUser = await User.findOne({ email }).session(session);
+
+      if (!existingUser) {
+        // create new user if user does not exist
+        const [newUser] = await User.create(
+          [
+            {
+              email,
+              name,
+              username: slugify(username || "", {
+                lower: true,
+                remove: /[*+~.()'"!:@]/g,
+                strict: true,
+                trim: true,
+              }),
+              image,
+            },
+          ],
+          { session },
+        );
+        // commit transaction
+        await session.commitTransaction();
+        return Response.json({ user: newUser }, { status: 201 });
+      } else {
+        // update user if user exists
+        await User.updateOne(
+          {
+            _id: existingUser._id,
+          },
+          {
+            $set: {
+              name,
+              image,
+            },
+          },
+        ).session(session);
+        // commit transaction
+        await session.commitTransaction();
+        return Response.json({ user: existingUser }, { status: 200 });
+      }
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      await session.endSession();
+    }
+  } catch (error: unknown) {
+    console.log(error);
+    return Response.json(
+      {
+        error: error instanceof Error ? error.message : "Internal Server Error",
+      },
+      { status: 500 },
+    );
+  }
+}
