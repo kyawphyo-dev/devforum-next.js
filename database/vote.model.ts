0c79de2177f1422c3b40884f29ea31c5912f66ef
Kyaw Phyo Win <kyawphyowin242965@gmail.com>
2026-06-05 23:12:42 +0700

    VoteUI

--- database/vote.model.ts
+++ database/vote.model.ts
@@ -1,42 +1,57 @@
 import { Schema, Document, models, model } from "mongoose";
 
 export interface IVote {
-  user: Schema.Types.ObjectId;
-  questionId?: Schema.Types.ObjectId;
-  answerId?: Schema.Types.ObjectId;
+  userId: Schema.Types.ObjectId;
+  targetId: Schema.Types.ObjectId;
+  targetType: "question" | "answer";
   voteType: "upvote" | "downvote";
 }
 
-export interface IVoteDoc extends IVote, Document {}
+export interface IVoteDoc extends Document, IVote {}
 
-const VoteSchema = new Schema(
+const VoteSchema = new Schema<IVoteDoc>(
   {
-    user: {
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    questionId: {
+
+    targetId: {
       type: Schema.Types.ObjectId,
-      ref: "Question",
+      required: true,
     },
-    answerId: {
-      type: Schema.Types.ObjectId,
-      ref: "Answer",
+
+    targetType: {
+      type: String,
+      enum: ["question", "answer"],
+      required: true,
     },
+
     voteType: {
       type: String,
       enum: ["upvote", "downvote"],
       required: true,
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  }
 );
 
-// Prevent duplicate votes by the same user on the same question or answer
-VoteSchema.index({ user: 1, question: 1 }, { unique: true, sparse: true });
-VoteSchema.index({ user: 1, answer: 1 }, { unique: true, sparse: true });
+// Prevent duplicate votes from the same user
+VoteSchema.index(
+  {
+    userId: 1,
+    targetId: 1,
+    targetType: 1,
+  },
+  {
+    unique: true,
+  }
+);
 
-const Vote = models?.Vote || model<IVoteDoc>("Vote", VoteSchema);
+const Vote =
+  models.Vote || model<IVoteDoc>("Vote", VoteSchema);
 
 export default Vote;
